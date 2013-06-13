d254479894695048b61ac7d485f323353e8f799c
Tyler Roussos <tr36@njit.edu>
2013-06-13 09:58:28 -0300

    Updated personal image factory function to support the new thing-id and version

--- src/biologis/HV/HealthRecordItem/PersonalImage.php
+++ src/biologis/HV/HealthRecordItem/PersonalImage.php
@@ -7,11 +7,12 @@ use biologis\HV\HealthRecordItemFactory;
 
 class PersonalImage extends HealthRecordItemData {
 
-    public static function createFromData($image) {
+    public static function createFromData($image, $thingId, $thingVersion) {
         $personalImage = HealthRecordItemFactory::getThing('Personal Image');
         $personalImage->getQp()->find('data-other')->text($image);
+        $personalImage->getQp()->find('thing-id')->text($thingId);
+        $personalImage->getQp()->find('thing-id')->attr("version-stamp", $thingVersion);
+
         return $personalImage;
     }
 }
-
-
