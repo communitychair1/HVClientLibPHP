d93b9e4e0bd210b483f2f94a101d6a46df95a617
nicholasjcostello <nicholasjcostello@gmail.com>
2013-06-25 11:34:48 -0400

    Updated Tracker Test
    Updated Tracker UnitTest to test our gathering data within a range of
    dates. (max date -> min date)

--- tests/biologis/HV/HVTrackerTest.php
+++ tests/biologis/HV/HVTrackerTest.php
@@ -9,7 +9,7 @@ use biologis\HV\HVClient;
 
 require_once("HVClientBaseTest.php");
 
-class HVGroupTest extends HVClientBaseTest
+class HVTrackerTest extends HVClientBaseTest
 {
 
     /**
@@ -91,10 +91,10 @@ class HVGroupTest extends HVClientBaseTest
         }
     }
     /**
-     * Test Tracker Request By Date 14
+     * Test Tracker Request By Date 30
      *
      *  Tests the return of all data related to sleep and emotional state
-     *  Filtered by a 14 day time stamp
+     *  Filtered by a 30 day time stamp
      */
     public function testTrackerRequestByDate30(){
 
@@ -150,6 +150,78 @@ class HVGroupTest extends HVClientBaseTest
                         $thing->{'sleep-pm'}->{'when'}->{'date'}->{'m'});
             }
         }
+    }
+
+    /**
+     * Test Tracker Request Max Min Date
+     *
+     *  Tests retrieving tracker data within a range of dates
+     */
+    public function testTrackerRequestMaxMinDate(){
+
+        //Create a timestamp 14 days in the past
+        $dateFilterStrMax = '-2 days';
+        $dateFilterStrMin = '-5 days';
+
+        $timeMax = date(DATE_ATOM, mktime(0,0,0,
+            date('m', strtotime($dateFilterStrMax)),
+            date('d', strtotime($dateFilterStrMax)),
+            date('Y', strtotime($dateFilterStrMax))));
+
+        $timeMin = date(DATE_ATOM, mktime(0,0,0,
+            date('m', strtotime($dateFilterStrMin)),
+            date('d', strtotime($dateFilterStrMin)),
+            date('Y', strtotime($dateFilterStrMin))));
+
+        //Create an XML filter using timestamp
+        $timeFilterMax = '<eff-date-max>'.$timeMax.'</eff-date-max>';
+        $timeFilterMin = '<eff-date-min>'.$timeMin.'</eff-date-min>';
+
+        //Init array's for request
+        $option = array();
+        $requestGroup = array();
+
+        //Populate the Request group
+        // Key = TypeName of Thing to request
+        // Value = filter on that thing request
+        $requestGroup["Sleep Related Activity"] = $timeFilterMin.$timeFilterMax;
+        $requestGroup["Emotional State"] = $timeFilterMin.$timeFilterMax;
+
+        //Make the request to health vault.
+        $response = $this->hv->getThings(
+            $requestGroup,
+            $this->recordId,
+            $option,
+            false
+        );
+
+
+        foreach($response as $thing)
+        {
+            if($thing->{'emotion'})
+            {
+                if($thing->{'emotion'}->{'when'}->{'date'}->{'m'} == date('m', strtotime($dateFilterStrMax)))
+                    $this->assertLessThanOrEqual(date('d', strtotime($dateFilterStrMax)),
+                        $thing->{'emotion'}->{'when'}->{'date'}->{'d'});
+                if($thing->{'emotion'}->{'when'}->{'date'}->{'m'} == date('m', strtotime($dateFilterStrMin)))
+                    $this->assertGreaterThanOrEqual(date('d', strtotime($dateFilterStrMin)),
+                        $thing->{'emotion'}->{'when'}->{'date'}->{'d'});
+            }
+            elseif($thing->{'sleep-pm'})
+            {
+                if($thing->{'sleep-pm'}->{'when'}->{'date'}->{'m'} == date('m', strtotime($dateFilterStrMax)))
+                    $this->assertLessThanOrEqual(date('d', strtotime($dateFilterStrMax)),
+                        $thing->{'sleep-pm'}->{'when'}->{'date'}->{'d'});
+                if($thing->{'emotion'}->{'when'}->{'date'}->{'m'} == date('m', strtotime($dateFilterStrMin)))
+                    $this->assertGreaterThanOrEqual(date('d', strtotime($dateFilterStrMin)),
+                        $thing->{'sleep-pm'}->{'when'}->{'date'}->{'d'});
+            }
+        }
+
+
+
 
     }
+
+
 }
