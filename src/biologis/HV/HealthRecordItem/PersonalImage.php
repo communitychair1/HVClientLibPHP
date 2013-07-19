43739bd9f1ae2ba169f0df01309c0e9dfdab2e27
troussos <tylerroussos@gmail.com>
2013-07-19 11:38:17 -0400

    Fixed issue with subsequent profile images.

--- src/biologis/HV/HealthRecordItem/PersonalImage.php
+++ src/biologis/HV/HealthRecordItem/PersonalImage.php
@@ -7,9 +7,13 @@ use biologis\HV\HealthRecordItemFactory;
 
 class PersonalImage extends HealthRecordItemData {
 
-    public static function createFromData($image) {
+    public static function createFromData($image, $thingId = null, $thingVersion = null) {
         $personalImage = HealthRecordItemFactory::getThing('Personal Image');
         $personalImage->getQp()->find('data-other')->text($image);
+        if($thingId != null)
+            $personalImage->getQp()->find('thing-id')->text($thingId);
+        if($thingVersion != null)
+            $personalImage->getQp()->find('thing-id')->attr("version-stamp", $thingVersion);
 
         return $personalImage;
     }
