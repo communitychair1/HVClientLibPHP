0da47b7a7f3170dd0c8c815b0f3863bdd11a7c73
nicholasjcostello <nicholasjcostello@gmail.com>
2013-07-30 16:24:27 -0400

    Edit for last commit

--- src/biologis/HV/PersonInfo.php
+++ src/biologis/HV/PersonInfo.php
@@ -10,26 +10,46 @@ namespace biologis\HV;
 
 use QueryPath\Query;
 
-class PersonInfo extends AbstractXmlEntity {
+/** PERSON INFO
+ * Class PersonInfo
+ * @package biologis\HV
+ */
+class PersonInfo extends AbstractXmlEntity
+{
 
-  public function __construct(Query $qp) {
-    $this->qp = $qp;
-  }
-
-  public function getRecordList() {
-    $records = array();
-    foreach ($this->qp->top()->find('record') as $record) {
-      $records[$record->attr('id')] = $record->text();
+    /** CONSTRUCTOR
+     * @param Query $qp
+     */
+    public function __construct(Query $qp)
+    {
+        $this->qp = $qp;
     }
-    return $records;
-  }
 
-    public function getRecordsFormatted(){
+    /** GET RECORD LIST
+     * @return array
+     * returns an array of user names and associated record id's
+     */
+    public function getRecordList()
+    {
+        $records = array();
+        foreach ($this->qp->top()->find('record') as $record) {
+            $records[$record->attr('id')] = $record->text();
+        }
+        return $records;
+    }
+
+    /** GET RECORDS FORMATTED
+     * @return array
+     * returns all records associated with the account formatted into an array
+     */
+    public function getRecordsFormatted()
+    {
         $records = array();
         $index = 0;
         $recordBranch = $this->qp->top()->branch()->find('record');
-        foreach($recordBranch as $record){
+        foreach ($recordBranch as $record) {
             $records[$index]['id'] = $record->attr('id');
+            $records[$index]['name'] = $record->attr('display-name');
             $records[$index]['rel-type'] = $record->attr('rel-type');
             $records[$index]['rel-name'] = $record->attr('rel-name');
             $index++;
@@ -37,28 +57,33 @@ class PersonInfo extends AbstractXmlEntity {
         return $records;
     }
 
-  public function getRecordById($id) {
-    $qpRecord = $this->qp->top()->branch()->find('record#' . $id);
-    $this->qp->top();
-    if ($qpRecord) {
-      return (object) array(
-        'id' => $id,
-        'record-custodian' => $qpRecord->attr('record-custodian'),
-        'rel-type' => $qpRecord->attr('rel-type'),
-        'rel-name' => $qpRecord->attr('rel-name'),
-        'auth-expires' => $qpRecord->attr('auth-expires'),
-        'display-name' => $qpRecord->attr('display-name'),
-        'state' => $qpRecord->attr('state'),
-        'date-created' => $qpRecord->attr('date-created'),
-        'max-size-bytes' => $qpRecord->attr('max-size-bytes'),
-        'size-bytes' => $qpRecord->attr('size-bytes'),
-        'app-record-auth-action' => $qpRecord->attr('app-record-auth-action'),
-        'app-specific-record-id' => $qpRecord->attr('app-specific-record-id'),
-        'location-country' => $qpRecord->attr('location-country'),
-        'date-updated' => $qpRecord->attr('date-updated'),
-      );
-    }
+    /** Get Record By Id
+     * @param $id
+     * @return null|object
+     */
+    public function getRecordById($id)
+    {
+        $qpRecord = $this->qp->top()->branch()->find('record#' . $id);
+        $this->qp->top();
+        if ($qpRecord) {
+            return (object)array(
+                'id' => $id,
+                'record-custodian' => $qpRecord->attr('record-custodian'),
+                'rel-type' => $qpRecord->attr('rel-type'),
+                'rel-name' => $qpRecord->attr('rel-name'),
+                'auth-expires' => $qpRecord->attr('auth-expires'),
+                'display-name' => $qpRecord->attr('display-name'),
+                'state' => $qpRecord->attr('state'),
+                'date-created' => $qpRecord->attr('date-created'),
+                'max-size-bytes' => $qpRecord->attr('max-size-bytes'),
+                'size-bytes' => $qpRecord->attr('size-bytes'),
+                'app-record-auth-action' => $qpRecord->attr('app-record-auth-action'),
+                'app-specific-record-id' => $qpRecord->attr('app-specific-record-id'),
+                'location-country' => $qpRecord->attr('location-country'),
+                'date-updated' => $qpRecord->attr('date-updated'),
+            );
+        }
 
-    return NULL;
-  }
+        return NULL;
+    }
 }
